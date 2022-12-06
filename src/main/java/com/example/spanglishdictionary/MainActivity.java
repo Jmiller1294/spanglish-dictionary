8e3633c2e1bfaf9b81af871866a28b1998c4154f
Justin Miller <54234620+Jmiller1294@users.noreply.github.com>
2022-12-06 12:34:21 -0500

    Add files via upload

--- src/main/java/com/example/spanglishdictionary/MainActivity.java
+++ src/main/java/com/example/spanglishdictionary/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.spanglishdictionary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ShareCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -15,9 +16,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
-
     public void startWordLookup(View view) {
         Intent intent = new Intent(MainActivity.this, WordLookupActivity.class);
+        startActivity(intent);
+    }
+    public void viewSavedEntries(View view){
+        Intent intent = new Intent(MainActivity.this,SavedWordsActivity.class);
+        startActivity(intent);
+    }
+    public void inviteFriend(View view){
+        String msg = "Download the app Spanglish Dictionary";
+        String mimeType = "text/plain";
+        ShareCompat.IntentBuilder
+                .from(this)
+                .setType(mimeType)
+                .setChooserTitle("Complete Action using: ")
+                .setText(msg)
+                .startChooser();
+    }
+    public void addWord(View view){
+        Intent intent = new Intent(MainActivity.this,AddWordActivity.class);
         startActivity(intent);
     }
 }
--- src/main/java/com/example/spanglishdictionary/SavedWordsActivity.java
+++ src/main/java/com/example/spanglishdictionary/SavedWordsActivity.java
@@ -11,4 +11,6 @@ public class SavedWordsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saved_words);
     }
+
+
 }
